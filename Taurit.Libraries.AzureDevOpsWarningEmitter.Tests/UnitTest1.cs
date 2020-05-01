b52f1472178a99d9a7fce4adefeec3995686b3e5
Paweł Bulwan <ppbu@demant-technology.com>
2020-05-01 17:29:31 +0200

    Add a Unit Test project

---
+++ Taurit.Libraries.AzureDevOpsWarningEmitter.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Taurit.Libraries.AzureDevOpsWarningEmitter.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
